69a8f7cc801ec4c8712ced5392c19a904fb77421
Chao Zhang <zhangchao6865@gmail.com>
2021-03-15 14:41:52 -0700

    Update FactorialCalculator.kt

--- library-kotlin/src/main/java/com/ncorti/kotlin/template/library/FactorialCalculator.kt
+++ library-kotlin/src/main/java/com/ncorti/kotlin/template/library/FactorialCalculator.kt
@@ -3,6 +3,7 @@ package com.ncorti.kotlin.template.library
 object FactorialCalculator {
 
 
+    
     tailrec fun computeFactorial(input: Int, temp: Long = 1L): Long =
         when {
             input < 0 -> error("Factorial is not defined for negative numbers adsfasdfd afdsa dsfaasdfasdf asdf ")
