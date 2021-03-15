df93d3260efe7e69c9d77851136ccb0de8b27131
GitHub Action <action@github.com>
2021-03-15 22:06:44 +0000

    Template cleanup

---
+++ library-kotlin/src/main/java/com/github/chao2zhang/codescanningplaygroundkotlin/library/FactorialCalculator.kt
@@ -0,0 +1,12 @@
+package com.github.chao2zhang.codescanningplaygroundkotlin.library
+
+object FactorialCalculator {
+
+
+    tailrec fun computeFactorial(input: Int, temp: Long = 1L): Long =
+        when {
+            input < 0 -> error("Factorial is not defined for negative numbers adsfasdfd afdsa dsfaasdfasdf asdf ")
+            input == 0 -> temp
+            else -> computeFactorial(input - 1, temp * input)
+        }
+}
