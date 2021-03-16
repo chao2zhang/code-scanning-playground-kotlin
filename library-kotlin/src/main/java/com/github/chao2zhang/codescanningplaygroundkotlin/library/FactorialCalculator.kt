d78c770865ac077701fabfa9a9ba1bd9df57ecf0
Chao Zhang <zhangchao6865@gmail.com>
2021-03-15 22:52:37 -0700

    Update FactorialCalculator.kt

--- library-kotlin/src/main/java/com/github/chao2zhang/codescanningplaygroundkotlin/library/FactorialCalculator.kt
+++ library-kotlin/src/main/java/com/github/chao2zhang/codescanningplaygroundkotlin/library/FactorialCalculator.kt
@@ -2,6 +2,7 @@ package com.github.chao2zhang.codescanningplaygroundkotlin.library
 
 object FactorialCalculator {
 
+
     tailrec fun computeFactorial(input: Int, temp: Long = 1L): Long =
         when {
             input < 0 -> error("Factorial is not defined for negative numbers adsfasdfd afdsa dsfaasdfasdf asdf ")
@@ -9,5 +10,6 @@ object FactorialCalculator {
             else -> computeFactorial(input - 1, temp * input)
         }
 
+ 
     fun a() = 42
 }
